92327924524b5b85436e64715b3dae023221c3af
Eli Pleaner <eliipleaner@gmail.com>
2022-10-03 13:29:14 -0700

    Remove boilerplate

--- src/components/react-mic.js
+++ src/components/react-mic.js
@@ -4,45 +4,50 @@ import { ReactMic } from 'react-mic';
 function Mic() {
   const [isRecording, setIsRecording] = useState(false);
 
-  // function submitForm(e) {
-  //   e.preventDefault();
-  //   const name = document.getElementById('name');
-  //   const files = document.getElementById('files');
-  //   const formData = new FormData();
-  //   formData.append('name', name.value);
-  //   for (let i = 0; i < files.files.length; i++) {
-  //     console.log(files.files[i]);
-  //     formData.append('files', files.files[i]);
-  //   }
-  //   console.log('hello', formData);
-  //   fetch('http://localhost:3001/upload_sound', {
-  //     method: 'POST',
-  //     body: formData,
-  //   })
-  //     .then((res) => console.log(res))
-  //     .catch((err) => ('Error occured', err));
-  // }
+  function submitForm(e) {
+    e.preventDefault();
+    const files = document.getElementById('soundBlob');
+
+    const formData = new FormData();
+
+    for (let i = 0; i < files.files.length; i++) {
+      console.log(files.files[i]);
+      formData.append('soundBlob', files.files[i]);
+    }
+
+    fetch('http://localhost:3001/upload_sound', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((res) => console.log(res))
+      .catch((err) => ('Error occured', err));
+  }
 
   const onData = (recordedBlob) => {
-    console.log('chunk of real-time data is: ', recordedBlob);
+    // console.log('chunk of real-time data is: ', recordedBlob);
   };
 
   const onStop = async (recordedBlob) => {
-    console.log('recordedBlob is: ', recordedBlob);
+    // console.log('recordedBlob is: ', recordedBlob);
 
     const formData = new FormData();
 
     recordedBlob.blob.lastModifiedDate = new Date();
     recordedBlob.blob.filename = 'recording.mp3';
 
-    formData.append('soundBlob', recordedBlob.blob, 'recording.mp3');
+    let file = new File([recordedBlob.blob], 'recording.mp3', {
+      type: 'audio/mpeg',
+    });
+    console.log(file);
+
+    formData.append('soundBlob', file);
 
     fetch('http://localhost:3001/upload_sound', {
       method: 'post',
       body: formData,
-      headers: new Headers({
-        enctype: 'multipart/form-data',
-      }),
+      // headers: new Headers({
+      //   enctype: 'multipart/form-data',
+      // }),
     })
       .then((res) => console.log(res))
       .catch((err) => console.log('Error occurred', err));
@@ -65,21 +70,14 @@ function Mic() {
       <button onClick={() => setIsRecording(false)} type='button'>
         Stop
       </button>
-      {/* <div className='container'>
+      <div className='container'>
         <form id='form' onSubmit={submitForm}>
-          <div className='input-group'>
-            <label htmlFor='name'>Your name</label>
-            <input name='name' id='name' placeholder='Enter your name' />
-          </div>
-          <div className='input-group'>
-            <label htmlFor='files'>Select files</label>
-            <input id='files' type='file' multiple />
-          </div>
+          <input id='soundBlob' type='file' />
           <button className='submit-btn' type='submit'>
             Upload
           </button>
         </form>
-      </div> */}
+      </div>
     </div>
   );
 }
